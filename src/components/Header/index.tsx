8d3527cc35ca549def9e334aeae0a6d1b702ebaf
EmersonGarrido <emersongarrido@hotmail.com.br>
2022-08-22 01:37:37 -0400

    🐛 fix: Alterado o a do HTML para Link Nextjs

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,35 @@
 import React from "react";
+import Link from "next/link";
 
 const Header: React.FC = () => {
   return (
     <div className="flex items-center justify-center w-full">
       <div className="flex items-center justify-center w-[1200px] p-6">
-      <div className="w-full flex items-center gap-3">
-       <a href="/"> <img src="/assets/logo.png" className="w-[50px] cursor-pointer" /></a>
-        <h1 className="text-[#343434]">|</h1>
-        <h1 className="text-[#8257E6]">Blog</h1>
-        <ul className=" hidden items-center justify-center gap-5 ml-8 text-[#B4B4B4] text-[16px]">
-          <li className="hover:cursor-pointer hover:text-[#fff]"> Back-end </li>
-          <li className="hover:cursor-pointer hover:text-[#fff]"> Front-end </li>
-          <li className="hover:cursor-pointer hover:text-[#fff]"> Mobile </li>
-        </ul>
+        <div className="w-full flex items-center gap-3">
+          <Link href="/">
+            <a>
+              <img src="/assets/logo.png" className="w-[50px] cursor-pointer" />
+            </a>
+          </Link>
+
+          <h1 className="text-[#343434]">|</h1>
+          <h1 className="text-[#8257E6]">Blog</h1>
+          <ul className=" hidden items-center justify-center gap-5 ml-8 text-[#B4B4B4] text-[16px]">
+            <li className="hover:cursor-pointer hover:text-[#fff]">Back-end</li>
+            <li className="hover:cursor-pointer hover:text-[#fff]">
+              Front-end
+            </li>
+            <li className="hover:cursor-pointer hover:text-[#fff]"> Mobile </li>
+          </ul>
+        </div>
+        <div className=" w-[30px] cursor-pointer">
+          <Link href="https://github.com/EmersonGarrido">
+            <a target="_blank">
+              <img src="/assets/github-brands.svg" />
+            </a>
+          </Link>
+        </div>
       </div>
-      <div className=" w-[30px] cursor-pointer"><a href="https://github.com/EmersonGarrido" target="_blank"><img src="/assets/github-brands.svg" /></a></div>
-    </div>
     </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 import { Header } from "components";
 
 const Home: NextPage = () => {
@@ -6,7 +7,9 @@ const Home: NextPage = () => {
     <div>
       <Header />
       <div className="w-full bg-black h-[250px] md:h-[450px] flex items-center flex-col p-4 md:p-0 justify-center">
-        <h1 className="text-[28px] font-bold md:text-[52px]">Blog do Emerson Garrido</h1>
+        <h1 className="text-[28px] font-bold md:text-[52px]">
+          Blog do Emerson Garrido
+        </h1>
         <span className="text-[14px] md:text-[18px] text-center">
           Conteúdos focados em desenvolvimento e tutoriais do <b>básico</b> ao{" "}
           <b>avançado.</b>
@@ -15,10 +18,18 @@ const Home: NextPage = () => {
       <div className="flex items-center justify-center p-4 font-thin text-[28px]">
         <h1>Em desenvolvimento...</h1>
       </div>
-     <div className="text-[14px] font-thin">
-     <span className="flex items-center justify-center">Todos os direitos reservados a </span>
-      <span className="flex items-center justify-center"><a href="https://emersongarrido.com.br"><b> Emerson Garrido </b> </a></span>
-     </div>
+      <div className="text-[14px] font-thin">
+        <span className="flex items-center justify-center">
+          Todos os direitos reservados a
+        </span>
+        <span className="flex items-center justify-center">
+          <Link href="https://emersongarrido.com.br">
+            <a>
+              <b> Emerson Garrido </b>
+            </a>
+          </Link>
+        </span>
+      </div>
     </div>
   );
 };
